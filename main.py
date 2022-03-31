34c71bf5b7c6efd65b9cadbedc9f1d09b669ad0f
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-03-31 09:00:44 +0530

    rename from alexa to bot

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from gtts import gTTS
 r = sr.Recognizer()
 
 
-def alexa_speak(audio_string):
+def bot_speak(audio_string):
     tts = gTTS(text=audio_string, lang='en')
     r = random.randint(1, 10000)
     audio_file = 'audio-'+str(r)+'.mp3'
@@ -21,7 +21,7 @@ def alexa_speak(audio_string):
 
 def record_audio(ask=False):
     if ask:
-        alexa_speak(ask)
+        bot_speak(ask)
     with sr.Microphone() as source:
         print("Say something")
         audio = r.listen(source)
@@ -29,33 +29,33 @@ def record_audio(ask=False):
         try:
             voice_data = r.recognize_google(audio)
         except sr.UnknownValueError:
-            alexa_speak("Sorry, your voice could not be recognized")
+            bot_speak("Sorry, your voice could not be recognized")
         except sr.RequestError:
-            alexa_speak("The speech service is down")
+            bot_speak("The speech service is down")
         return (voice_data)
 
 
 def respond(voice_data):
     if "hello" in voice_data:
-        alexa_speak("Hello!")
+        bot_speak("Hello!")
         
     if "what is your name" in voice_data:
-        alexa_speak("My name is Alexa")
+        bot_speak("My name is Bot!")
 
     if 'what time is it' in voice_data:
-        alexa_speak(ctime())
+        bot_speak(ctime())
         
     if "search" in voice_data:
         search = record_audio("What do you want to search for?")
         url = "https://google.com/search?q=" + search
         webbrowser.get().open(url)
-        alexa_speak("I found this for " + search)
+        bot_speak("I found this for " + search)
 
     if 'find location' in voice_data:
         location = record_audio("What location do you want to search for?")
         url = "https://google.co.in/maps/place/"  + location + '/&amp;'
         webbrowser.get().open(url)
-        alexa_speak("Here is the location of " + location)
+        bot_speak("Here is the location of " + location)
         
     if 'exit' in voice_data:
         exit()
